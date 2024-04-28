a05da8a5b2fffea19e1d9b5e388ba17d90ae8ce6
Eugene Kaushnian <kefbox@gmail.com>
2024-04-28 14:10:29 -0400

    Notification service

--- python/src/notification/consumer.py
+++ python/src/notification/consumer.py
@@ -1,21 +1,10 @@
-import pika
-import sys
 import os
-import time
-from pymongo import MongoClient
-import gridfs
-from convert import to_mp3
+import sys
+import pika
+from send import email
 
 
 def main():
-    client = MongoClient("host.minikube.internal", 27017)
-    db_videos = client.videos
-    db_mp3s = client.mp3s
-
-    # gridfs
-    fs_videos = gridfs.GridFS(db_videos)
-    fs_mp3s = gridfs.GridFS(db_mp3s)
-
     # rabbitmq connection
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host="rabbitmq")
@@ -23,14 +12,14 @@ def main():
     channel = connection.channel()
 
     def callback(ch, method, properties, body):
-        err = to_mp3.start(body, fs_videos, fs_mp3s, ch)
+        err = email.notification(body)
         if err:
             ch.basic_nack(delivery_tag=method.delivery_tag)
         else:
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
     channel.basic_consume(
-        queue=os.environ.get("VIDEO_QUEUE"), on_message_callback=callback
+        queue=os.environ.get("MP3_QUEUE"), on_message_callback=callback
     )
 
     print("Waiting for messages. To exit press Ctrl+C")
